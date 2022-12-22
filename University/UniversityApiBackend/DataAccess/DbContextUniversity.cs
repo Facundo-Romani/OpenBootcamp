03a245b4640c62617f7909cf88bfc3dc13310dd7
Facundo-Romani <facundoromani90@gmail.com>
2022-12-22 20:58:28 -0300

    University

---
+++ University/UniversityApiBackend/DataAccess/DbContextUniversity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class DbContextUniversity : DbContext
+    {
+        // DB Context
+        public DbContextUniversity(DbContextOptions<DbContextUniversity> options) : base(options)
+        {
+
+        }
+
+        // Mapear Tablas
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Chapter> chapters { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ University/UniversityApiBackend/Models/DataModels/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string CreatedBy { get; set; } = string.Empty; // Por defecto NULL.
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime? UpdatedAt { get; set; }
+        public string DeletedBy { get; set; } = string.Empty;
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
